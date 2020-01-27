3fdd9298b1a9781be291e7c5b38ef8dd8481b7ae
yourUsername <yourEmail@domain.com>
2020-01-26 19:37:16 -0500

    gg

---
+++ demo/src/main/java/com/example/demo/sum.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+public class sum {
+public static void main(String[]args) {
+
+    int add =22;
+// do while loop and while loop are going one more step to it
+while(add<120)
+{
+    add = add+22;
+
+System.out.println(""+ add);
+
+
+
+
+}
+}
+
+}
